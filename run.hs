74f42e0d0ddaa9cd8b0d2e41173bbb57c19e3d91
Tikhon Jelvis <tikhon@jelv.is>
2014-01-20 04:50:47 -0800

    Fixed a bug in pop_s in instrs.sk

--- run.hs
+++ run.hs
@@ -27,32 +27,50 @@ instance IsString [Opcode] where fromString = map (\ (Opcode o) -> o) . read
 deriving instance Typeable Opcode
 deriving instance Data Opcode
 
-main = do writeFile "generated-sketch-new.sk" $ harness "over over or a! 5 and a or" 4
-          -- result <- readProcess "./sketch" ["generated-sketch.sk"] ""
-          -- print $ parseSk result
+supported' :: [Opcode]
+supported' = [ FetchP
+            , FetchPlus
+            , FetchB
+            , Fetch
+            , StoreP
+            , StorePlus
+            , StoreB
+            , Store
+            , Times2
+            , Div2
+            , Not
+            , Plus
+            , And
+            , Or
+            , Drop
+            , Dup
+            , Pop
+            , Over
+            , ReadA
+            , Nop
+            , Push
+            , SetB
+            , SetA
+            ]
 
-parseSk :: String -> [Maybe Constr]
-parseSk = map (readInstr . takeWhile isLetter . dropWhile (not . isLetter)) .
-          takeWhile (not . isInfixOf "for") .
-          filter (not . isInfixOf "bit[18]") .
-          drop 7 .
-          takeWhile (/= "}") .
-          dropWhile (not . isPrefixOf "void sketch") .
-          lines
-  where readInstr = readConstr $ dataTypeOf Or
+supported = [And, Or]
 
-harness :: Program -> Int -> String
-harness spec n = [sketch|
+main = generate "over over or a! and a or" 4
+
+generate problem len = writeFile "generated-sketch.sk" $ harness problem len 18
+
+harness :: Program -> Int -> Int -> String
+harness spec length bits = [sketch|
 
 include "instrs.sk";
 pragma options "--bnd-int-range 1000";
 
 struct Ret {
-  bit[18] s;
-  bit[18] t;
+  bit[$bitSize] s;
+  bit[$bitSize] t;
 }
            
-|Ret| spec(bit[18] t_reg, bit[18] s_reg) {
+|Ret| spec(bit[$bitSize] t_reg, bit[$bitSize] s_reg) {
   reset();
   s.t = t_reg;
   s.s = s_reg;
@@ -60,35 +78,36 @@ struct Ret {
   return |Ret|(s = s.s, t = s.t);
 }
 
-|Ret| sketch(bit[18] t_reg, bit[18] s_reg) implements spec {
+|Ret| sketch(bit[$bitSize] t_reg, bit[$bitSize] s_reg) implements spec {
   reset();
   s.t = t_reg;
   s.s = s_reg;
-  bit[18] ignore = 0;
+  bit[$bitSize] ignore = 0;
   $holes
   return |Ret|(s = s.s, t = s.t);
 }
 
 |]
-  where specProgram = intercalate ";\n  " $ map call spec
-        holes = genHoles n
+  where specProgram = intercalate ";\n  " $ map (call bits) spec
+        bitSize = show bits
+        holes = genHoles length
 
 callOpcode :: Opcode -> String
 callOpcode instr = let (f:rest) = showConstr $ toConstr instr in toLower f : rest ++ "()"
 
-callLiteral :: F18Word -> String
-callLiteral = printf "loadLiteral({%s})" . toBits
+callLiteral :: Int -> F18Word -> String
+callLiteral bitSize = printf "loadLiteral(s, {%s})" . toBits
   where toBits n = intercalate "," . pad $ showIntAtBase 2 (head . show) n ""
-        pad ls | length ls > 18 = return <$> ls
-               | otherwise      = return <$> replicate (18 - length ls) '0' ++ ls
+        pad ls | length ls > bitSize = return <$> ls
+               | otherwise = return <$> replicate (bitSize - length ls) '0' ++ ls
 
-call :: Instruction -> String
-call (Opcode op) = callOpcode op
-call (Number n)  = callLiteral n
+call :: Int -> Instruction -> String
+call _ (Opcode op)      = callOpcode op
+call bitSize (Number n) = callLiteral bitSize n
 
 genHoles :: Int -> String
 genHoles n = drop 2 . unlines $ replicate n [sketch|
-  ignore = {| $opcodeCalls | loadLiteral(??) |};
+  ignore = {| $opcodeCalls |};
 |]
-  where opcodeCalls = intercalate " | " . map callOpcode . filter supported $ opcodes
-        supported opcode = not (isJump opcode) && opcode /= MultiplyStep && opcode /= Ret
+  where opcodeCalls = intercalate " | " . map callOpcode . filter isSupported $ opcodes
+        isSupported = (`elem` supported')
