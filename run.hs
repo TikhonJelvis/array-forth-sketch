d11568b4b2079bcb274c3d279e7ed97a6d9afab5
Tikhon Jelvis <tikhon@jelv.is>
2014-01-21 18:54:49 -0800

    Refactored a bit and added support for literal holes back in.

--- run.hs
+++ run.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE NamedFieldPuns     #-}
 {-# LANGUAGE OverloadedStrings  #-}
 {-# LANGUAGE QuasiQuotes        #-}
 {-# LANGUAGE StandaloneDeriving #-}
@@ -27,8 +28,29 @@ instance IsString [Opcode] where fromString = map (\ (Opcode o) -> o) . read
 deriving instance Typeable Opcode
 deriving instance Data Opcode
 
-supported' :: [Opcode]
-supported' = [ FetchP
+-- | Settings that control how the problem gets synthesized.
+data Settings =
+  Settings { supportedOpcodes :: [Opcode]
+           , literalHoles     :: Bool      -- ^ allow literal numbers in sketch?
+           , size             :: Int       -- ^ number of holes in sketch
+           , bits             :: Int       -- ^ number of bits in Forth words
+           }
+-- IMPORTANT: Right now, you have to manually update instrs.sk with the bit size!
+
+defaults :: Settings
+defaults = Settings { supportedOpcodes = supported
+                    , literalHoles     = True
+                    , size             = 6
+                    , bits             = 18
+                    }
+
+-- | The usual set of supported instructions. Pretty much anything
+--   without a literal address encoded in the instruction.
+-- 
+--   MultiplyStep is also not supported, but it should be in the
+--   future.
+supported :: [Opcode]
+supported = [ FetchP
             , FetchPlus
             , FetchB
             , Fetch
@@ -53,14 +75,12 @@ supported' = [ FetchP
             , SetA
             ]
 
-supported = [And, Or]
+main :: IO ()
+main = writeFile "generated-sketch.sk" $ harness settings "over over or a! and a or"
+  where settings = defaults { size = 4 } -- this is how you can "modify" the default settings
 
-main = generate "over over or a! and a or" 4
-
-generate problem len = writeFile "generated-sketch.sk" $ harness problem len 18
-
-harness :: Program -> Int -> Int -> String
-harness spec length bits = [sketch|
+harness :: Settings -> Program -> String
+harness settings@Settings { size, bits } spec = [sketch|
 
 include "instrs.sk";
 pragma options "--bnd-int-range 1000";
@@ -90,7 +110,7 @@ struct Ret {
 |]
   where specProgram = intercalate ";\n  " $ map (call bits) spec
         bitSize = show bits
-        holes = genHoles length
+        holes = genHoles settings size
 
 callOpcode :: Opcode -> String
 callOpcode instr = let (f:rest) = showConstr $ toConstr instr in toLower f : rest ++ "()"
@@ -105,9 +125,11 @@ call :: Int -> Instruction -> String
 call _ (Opcode op)      = callOpcode op
 call bitSize (Number n) = callLiteral bitSize n
 
-genHoles :: Int -> String
-genHoles n = drop 2 . unlines $ replicate n [sketch|
-  ignore = {| $opcodeCalls |};
+genHoles :: Settings -> Int -> String
+genHoles Settings { supportedOpcodes, literalHoles } n =
+  drop 2 . unlines $ replicate n [sketch|
+  ignore = {| $opcodeCalls $literalHole |};
 |]
-  where opcodeCalls = intercalate " | " . map callOpcode . filter isSupported $ opcodes
-        isSupported = (`elem` supported')
+  where opcodeCalls = intercalate " | " $ map callOpcode supportedOpcodes
+        literalHole | literalHoles = " | loadLiteral(??)"
+                    | otherwise    = ""
