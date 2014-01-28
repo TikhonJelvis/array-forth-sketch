91865e4c02cf22541a16e7eb1a469e0865f40db6
Tikhon Jelvis <tikhon@jelv.is>
2014-01-27 23:11:50 -0800

    Simplified and documented the conitions file.

--- src/Conditions.hs
+++ src/Conditions.hs
@@ -1,4 +1,10 @@
 {-# LANGUAGE QuasiQuotes #-}
+-- | This module deals with customizing which inputs and outputs the
+--   synthesis program considers.
+--  
+--   You can currently consider any registers (except i), some number
+--   of elements from the top of either stack or some number of
+--   elements in memory.
 module Conditions where
 
 import           Control.Applicative
@@ -19,28 +25,33 @@ data Condition = Register Register
                | Array Array Int -- ^ Stack or register, with the number of entries to inspect.
                deriving (Show, Eq)
 
-fieldName :: Int -> Condition -> String
-fieldName bitSize (Register reg)   =
-  printf "bit[%d] %s;" bitSize $ toLower <$> show reg
-fieldName bitSize (Array arr size) =
-  printf "bit[%d][%d] %s;" bitSize size $ toLower <$> show arr
+-- | Displays a register or array as a suitable variable name.
+name :: Condition -> String
+name (Register reg) = toLower <$> show reg
+name (Array arr _)  = toLower <$> show arr
 
-retStruct :: Int -> [Condition] -> String
-retStruct bitSize conditions = [sketch|
-struct Ret {
-$fields
-}
-|]
-  where fields = init . unlines $ ("  " ++) . fieldName bitSize <$> conditions
+-- | Produce a literal comma-separated list using some function over conditions.
+list :: (Condition -> String) -> [Condition] -> String
+list f = intercalate ", " . map f
 
-retStatement :: Int -> [Condition] -> String
-retStatement bitSize conditions = [sketch|
-return |Ret|($populations);
-|]
-  where populations = intercalate ", " $ populate <$> conditions
+-- | The fields of the Ret struct definition.
+fields :: [Condition] -> String
+fields = init . unlines . (map $ ("  " ++) . go)
+  where go reg@Register{}     = printf "bit[BIT_SIZE] %s" $ name reg
+        go arr@(Array _ size) = printf "bit[BIT_SIZE][%d] %s" size $ name arr
 
-populate :: Condition -> String
-populate (Register reg) =
-  let name = toLower <$> show reg in printf "%s = s.%s" name name
-populate (Array arr size) =
-  let name = toLower <$> show arr in printf "%s = s.%s[0::%d]" name name size
+-- | Arguments to the sketch and spec functions.
+arguments :: [Condition] -> String
+arguments = list $ printf "bit[BIT_SIZE] %s_input" . name
+
+-- | Assignments to the starting state (s).
+fieldAssignments :: [Condition] -> String
+fieldAssignments = list go
+  where go reg@Register{}     = printf "s.%s = %s_input" (name reg) (name reg)
+        go arr@(Array _ size) = printf "s.%s = %s_input[0::%d]" (name arr) (name arr) size
+
+-- | Assignements to the returned |Ret| struct.
+returnedValues :: [Condition] -> String
+returnedValues = list go
+  where go reg@Register{}     = printf "%s = s.%s" (name reg) (name reg)
+        go arr@(Array _ size) = printf "%s = s.%s[0::%d]" (name arr) (name arr) size
