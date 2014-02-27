9afc360be4c7e5c5bd4b7a2ce5479af3a34615d1
Tikhon Jelvis <tikhon@jelv.is>
2014-02-27 00:52:21 -0800

    My test cases actually use the NativeProgram format, so I had to convert them.

--- src/Testing.hs
+++ src/Testing.hs
@@ -1,11 +1,13 @@
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE QuasiQuotes  #-}
 -- | A few utilities that generate test functions for our sketch code.
 module Testing where
 
 import           Data.Functor                ((<$>))
 import           Data.List                   (intercalate)
 
-import           Language.ArrayForth.Program (Program)
+import           Language.ArrayForth.NativeProgram (NativeProgram)
+import           Language.ArrayForth.Program (Program, fromNative)
 
 import           Text.Printf                 (printf)
 
@@ -23,15 +25,15 @@ render :: Assert -> String
 render (c₁ := c₂) = printf "assert %s == %s" (toExpr c₁) (toExpr c₂)
   where toExpr reg@Register{}  = "s." ++ toName reg
         toExpr arr@(Array _ i) = printf "s.%s[%d]" (toName arr) i :: String
-        toExpr (Value n)       = show n
+        toExpr (Value n)       = printf "{%s}" $ toBits 18 n
 render (Unchanged conditions) = intercalate ";\n  " $ assertUnchanged <$> conditions
   where assertUnchanged condition = render $ condition := 0
 
 -- | Generates a test function with the given name (in the form
 --   'test_<name>') that checks all the assertions against the result
 --   of running the spec F18A program.
-test :: String -> Program -> [Assert] -> String
-test name spec assertions = [sketch|
+test :: String -> NativeProgram -> [Assert] -> String
+test name (fromNative -> spec) assertions = [sketch|
 harness void test_$name() {
   reset();
   $program;
